23d47c86f898174545d1e965ac34fda7711af3e1
xyTom <75466119+xyTom@users.noreply.github.com>
2021-11-07 01:54:33 +0800

    Add custom theme, hide Referer header and cors setting #16
    Add a config which allow you to customize some behaviour of the Url-Shorten-Worker. You can set custom theme by modify the config.theme. There are two theme you can choose default and urlcool. Set config.theme to "theme/urlcool" to use the urlcool theme. Leave the config.theme to be blank to use the default theme. Also you can use config to control the CORS behaviour and HTTP Referer header.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const config = {
+no_ref: "off", //Control the HTTP referrer header, if you want to create an anonymous link that will hide the HTTP Referer header, please set to "on" .
+theme:"",//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
+cors: "on",//Allow Cross-origin resource sharing for API requests.
+}
+
 const html404 = `<!DOCTYPE html>
 <body>
   <h1>404 Not Found.</h1>
   <p>The url you visit is not found.</p>
+  <a href="https://github.com/xyTom/Url-Shorten-Worker/" target="_self">Fork me on GitHub</a>
 </body>`
 
+let response_header={
+  "content-type": "text/html;charset=UTF-8",
+} 
+
+if (config.cors=="on"){
+  response_header={
+  "content-type": "text/html;charset=UTF-8",
+  "Access-Control-Allow-Origin":"*",
+  "Access-Control-Allow-Methods": "POST",
+  }
+}
 
 async function randomString(len) {
 　　len = len || 6;
@@ -33,7 +51,7 @@ async function save_url(URL){
     let is_exist=await LINKS.get(random_key)
     console.log(is_exist)
     if (is_exist == null)
-        return await LINKS.put(random_key, URL),random_key
+        return await LINKS.put(random_key, URL, {expirationTtl: 86400}),random_key
     else
         save_url(URL)
 }
@@ -44,37 +62,21 @@ async function handleRequest(request) {
     console.log(req["url"])
     if(!await checkURL(req["url"])){
     return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
+      headers: response_header,
     })}
     let stat,random_key=await save_url(req["url"])
     console.log(stat)
     if (typeof(stat) == "undefined"){
       return new Response(`{"status":200,"key":"/`+random_key+`"}`, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
+      headers: response_header,
     })
     }else{
       return new Response(`{"status":200,"key":": Error:Reach the KV write limitation."}`, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
+      headers: response_header,
     })}
   }else if(request.method === "OPTIONS"){  
       return new Response(``, {
-      headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods": "POST",
-      },
+      headers: response_header,
     })
 
   }
@@ -84,7 +86,7 @@ async function handleRequest(request) {
   console.log(path)
   if(!path){
 
-    const html= await fetch("https://cdn.jsdelivr.net/gh/xyTom/Url-Shorten-Worker@gh-pages/index.html")
+    const html= await fetch("https://xytom.github.io/Url-Shorten-Worker/"+config.theme+"/index.html")
     
     return new Response(await html.text(), {
     headers: {
@@ -98,7 +100,18 @@ async function handleRequest(request) {
 
   const location = value
   if (location) {
-    return Response.redirect(location, 302)
+    if (config.no_ref=="on"){
+      let no_ref= await fetch("https://xytom.github.io/Url-Shorten-Worker/no-ref.html")
+      no_ref=await no_ref.text()
+      no_ref=no_ref.replace(/{Replace}/gm, location)
+      return new Response(no_ref, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    })
+    }else{
+      return Response.redirect(location, 302)
+    }
     
   }
   // If request not in kv, return 404
