818cf4503630deedd29057ed49bb2c6f7172b2e9
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-07-25 23:32:33 +0800

    customShortURL

--- main.js
+++ main.js
@@ -10,9 +10,7 @@ let res
     fetch(window.location.pathname, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url: document.querySelector("#text").value,
-                             data: JSON.stringify({ custom_shorturl: document.querySelector("#customs-shorturl").value })
-                            })
+      body: JSON.stringify({ url: document.querySelector("#text").value, customShortURL: document.querySelector("#customShortURL").value })
     }).then(function(response) {
       return response.json();
     })
