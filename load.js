86a5ff0e48b8f78af44c2bdff368a5e647a88e0b
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-07-31 04:36:35 +0800

    localStorage

--- load.js
+++ load.js
@@ -1,1 +1,13 @@
+$(function () {
+    var len = localStorage.len;
+    for (; len >= 0; len--) {
+        keyShortURL=localStorage.key(len);
+        valueLongURL=localStorage.getItem(keyShortURL);
 
+        urlList = document.querySelector("#urlList")
+        var child = document.createEtement('li')
+        var text = document.createTextNode(keyShortURL + " " + valueLongURL)
+        child.appendChild(text)
+        urlPair.append(child)
+    }
+})
