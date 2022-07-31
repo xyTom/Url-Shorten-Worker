0319b8ab11ffcc216beb8599e63c3e3417c914f2
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-07-31 12:35:36 +0800

    function loadUrlList() {

--- load.js
+++ load.js
@@ -1,3 +1,4 @@
+function loadUrlList() {
     var len = localStorage.len
     console.log(len)
     for (; len >= 0; len--) {
@@ -10,3 +11,4 @@
         child.appendChild(text)
         urlPair.append(child)
     }
+}
