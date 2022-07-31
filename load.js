4ebc35fd6947db6772ef59d88ef897a1155595a3
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-07-31 17:38:38 +0800

    t

--- load.js
+++ load.js
@@ -1,21 +1,21 @@
 function loadUrlList() {
-    alert("Load url list from localStorage!")
-    
+    alert("Load url list from localStorage!")    
+/*
     urlList = document.querySelector("#urlList")
-    var child = document.createElement('li')
-    var text = document.createTextNode("loadUrlList")
+    let child = document.createElement('li')
+    let text = document.createTextNode("loadUrlList")
     child.appendChild(text)
     urlList.append(child)
-
-    var len = localStorage.len
-    console.log(len)
+*/
+    let len = localStorage.len
+    console.log(+len)
     for (; len >= 0; len--) {
         keyShortURL=localStorage.key(len)
         valueLongURL=localStorage.getItem(keyShortURL)
 
         urlList = document.querySelector("#urlList")
-        var child = document.createElement('li')
-        var text = document.createTextNode(keyShortURL + " " + valueLongURL)
+        let child = document.createElement('li')
+        let text = document.createTextNode(keyShortURL + " " + valueLongURL)
         child.appendChild(text)
         urlList.append(child)
     }
