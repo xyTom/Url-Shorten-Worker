14df0cb38b18862b40cd170ff35b1986b24bf15e
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-08-04 01:44:12 +0800

    child.classList.add("list-group-item")

--- main-demo.js
+++ main-demo.js
@@ -32,12 +32,9 @@ function shorturl() {
         // add to urlList on the page
         let urlList = document.querySelector("#urlList")
         let child = document.createElement('li')
-        // let text = document.createTextNode(keyShortURL + " " + valueLongURL)
-        // child.appendChild(text)
-        //child.append(keyShortURL + " " + valueLongURL)
-        //child.setAttribute("class", "list-group-item")
-        child.className = "list-group-item"
-        // child.classList.add("list-group-item")
+        let text = document.createTextNode(keyShortURL + " " + valueLongURL)
+        child.appendChild(text)
+        child.classList.add("list-group-item")
         urlList.append(child)
       }
 
