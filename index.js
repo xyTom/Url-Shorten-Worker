4ee8546481753a94a974bc02ce1106a113094391
xyTom <75466119+xyTom@users.noreply.github.com>
2022-10-20 18:49:23 -0500

    Add Google Safe Browsing feature.
    Using the Google Safe Browsing API (https://developers.google.com/safe-browsing) v4 to check the URL is safe or not before redirect.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const config = {
 no_ref: "off", //Control the HTTP referrer header, if you want to create an anonymous link that will hide the HTTP Referer header, please set to "on" .
 theme:"",//Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
 cors: "on",//Allow Cross-origin resource sharing for API requests.
-unique_link:false,//If it is true, the same long url will be shorten into the same short url
+unique_link:true,//If it is true, the same long url will be shorten into the same short url
 custom_link:false,//Allow users to customize the short url.
+safe_browsing_api_key: "" //Enter Google Safe Browsing API Key to enable url safety check before redirect.
 }
 
 const html404 = `<!DOCTYPE html>
@@ -81,6 +82,25 @@ async function is_url_exist(url_sha512){
     return is_exist
   }
 }
+async function is_url_safe(url){
+
+  let raw = JSON.stringify({"client":{"clientId":"Url-Shorten-Worker","clientVersion":"1.0.7"},"threatInfo":{"threatTypes":["MALWARE","SOCIAL_ENGINEERING","POTENTIALLY_HARMFUL_APPLICATION","UNWANTED_SOFTWARE"],"platformTypes":["ANY_PLATFORM"],"threatEntryTypes":["URL"],"threatEntries":[{"url":url}]}});
+
+  let requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+  };
+
+  result = await fetch("https://safebrowsing.googleapis.com/v4/threatMatches:find?key=AIzaSyB9hnLmYPihUefkSl9Mnxui35NDCJVw650", requestOptions)
+  result = await result.json()
+  console.log(result)
+  if (Object.keys(result).length === 0){
+    return true
+  }else{
+    return false
+  }
+}
 async function handleRequest(request) {
   console.log(request)
   if (request.method === "POST") {
@@ -149,6 +169,18 @@ async function handleRequest(request) {
   
 
   if (location) {
+    if (config.safe_browsing_api_key){
+      if(!(await is_url_safe(location))){
+        let warning_page = await fetch("https://xytom.github.io/Url-Shorten-Worker/safe-browsing.html")
+        warning_page =await warning_page.text()
+        warning_page = warning_page.replace(/{Replace}/gm, location)
+        return new Response(warning_page, {
+          headers: {
+            "content-type": "text/html;charset=UTF-8",
+          },
+        })
+      }
+    }
     if (config.no_ref=="on"){
       let no_ref= await fetch("https://xytom.github.io/Url-Shorten-Worker/no-ref.html")
       no_ref=await no_ref.text()
